dc5c7faf4702ef136d9d766658fd5d43a87f51a6
James Anderson <jamesanderson@benefex.co.uk>
2017-10-02 12:00:02 +0100

    Add DACPAC deployment scripts

---
+++ CMD/DACPAC_Deployer.bat
@@ -0,0 +1,65 @@
+@ECHO OFF
+CLS
+TITLE Deploy the DACPAC
+SETLOCAL EnableExtensions EnableDelayedExpansion
+
+:: Set environment variables ########################
+
+SET SQLPackagePath="C:\program files\microsoft sql server\140\DAC\bin"
+SET DACPAC_Path="Path to DACPAC file"
+ 
+SET DatabaseName=Communications
+SET TargetServer=LocalHost
+
+SET "OutputPath=C:\Temp\DACPAC\"
+IF NOT %OutputPath:~-1% == \ SET OutputPath=%OutputPath%\
+
+SET "DriftReportPath=%OutputPath%%DatabaseName% Drift Report.xml"
+SET "DeployReportPath=%OutputPath%%DatabaseName% Deploy Report.xml"
+SET "DeployScriptPath=%OutputPath%%DatabaseName% Deployment Script.sql"
+SET "DeployDiagnosticsPath=%OutputPath%%DatabaseName% Deployment Diagnostics.txt"
+
+:: ##################################################
+
+CD %SQLPackagePath%
+
+:: Only run drift report if the database exists
+FOR /F %%i IN ('SQLCMD -S %TargetServer% -h-1 -Q "SET NOCOUNT ON; SELECT name from sys.databases WHERE name='%DatabaseName%'"') DO (
+	:: Generate drift report
+	SQLPackage.exe /Action:DriftReport /TargetServerName:"%TargetServer%" /TargetDatabaseName:"%DatabaseName%" /OverwriteFiles:True /OutputPath:"%DriftReportPath%" /Quiet:True
+	IF %ERRORLEVEL% NEQ 0 GOTO DriftReportError
+)
+
+SQLPackage.exe /Action:DeployReport /SourceFile:"%DACPAC_Path%" /TargetServerName:"%TargetServer%" /TargetDatabaseName:"%DatabaseName%" /OverwriteFiles:True /OutputPath:"%DeployReportPath%" /Properties:DropObjectsNotInSource=True /Quiet:True 
+IF %ERRORLEVEL% NEQ 0 GOTO DeployReportError
+
+SQLPackage.exe /Action:Script /SourceFile:"%DACPAC_Path%" /TargetServerName:"%TargetServer%" /TargetDatabaseName:"%DatabaseName%" /OverwriteFiles:True /OutputPath:"%DeployScriptPath%" /Properties:DropObjectsNotInSource=True /Quiet:True 
+IF %ERRORLEVEL% NEQ 0 GOTO ScripttError
+
+SQLPackage.exe /Action:Publish /SourceFile:"%DACPAC_Path%" /TargetServerName:"%TargetServer%" /TargetDatabaseName:"%DatabaseName%" /OverwriteFiles:True /DiagnosticsFile:"%DeployDiagnosticsPath%" /Properties:DropObjectsNotInSource=True /Quiet:True
+IF %ERRORLEVEL% NEQ 0 GOTO PublishError
+
+GOTO End
+
+:DriftReportError
+ECHO Failed generating drift report.
+ECHO Canceling deployment.
+EXIT /b 1
+
+:DeployReportError
+ECHO Failed generating deployment report.
+ECHO Canceling deployment.
+EXIT /b 1
+
+:ScripttError
+ECHO Failed generating deployment script.
+ECHO Canceling deployment.
+EXIT /b 1
+
+:PublishError
+ECHO Failed publishing database. 
+ECHO Canceling deployment.
+EXIT /b 1
+
+:End
+ECHO Deployment complete.
---
+++ Powershell/DACPAC_Deployer.ps1
@@ -0,0 +1,128 @@
+﻿cls
+# Set environment variables ########################
+
+## Needs work on catching errors from SQLPackage.exe and treating them as terminating errors
+
+$SQLPackagePath = "C:\program files\microsoft sql server\140\DAC\bin"
+$DACPAC_Path = "Path to DACPAC file"
+
+$DatabaseName = "Communications"
+$TargetServer = "LocalHost"
+
+$DriftReportPath = "C:\Temp\$DatabaseName Drift Report.xml"
+$DeployReportPath = "C:\Temp\$DatabaseName Deploy Report.xml"
+$DeployScriptPath = "C:\Temp\$DatabaseName Deployment Script.sql"
+$DeployDiagnosticsPath = "C:\Temp\$DatabaseName Deployment Diagnostics.txt"
+
+#####################################################
+
+$Result = 0
+
+# Add path for SQLPackage.exe
+try {
+    IF (-not ($env:Path).Contains($SQLPackagePath)) { 
+        $env:path = $env:path + ";$SQLPackagePath;" 
+    }
+}
+catch {
+    $Result = 1
+    Write-Error ("Failed adding SQLPackage to PATH: " + $errortext + $_)
+    Exit 1
+}
+
+if ($Result -eq 0) {
+    try {
+        # Generate drift report
+
+        Invoke-Command -ScriptBlock {
+            try {
+                SQLPackage /Action:DriftReport `
+                /TargetServerName:$TargetServer `
+                /TargetDatabaseName:$DatabaseName `
+                /OverwriteFiles:True `
+                /OutputPath:$DriftReportPath `
+                /Quiet:True
+            }
+            cat
+        } -ErrorAction Stop
+
+        #[xml]$x = gc -Path $DriftReportPath;
+        #$x.DeploymentReport.Operations.Operation |
+        #% -Begin {$a=@();} -process {$name = $_.name; $_.Item | %  {$r = New-Object PSObject -Property @{Operation=$name; Value = $_.Value; Type = $_.Type} ; $a += $r;} }  -End {$a}
+    }
+    catch {
+        $Result = 1
+        Write-Error ("Failed to generate deployment report: " + $_)
+        Exit 1
+    }
+} 
+
+if ($Result -eq 0) {
+    try {
+        # Generate deployment report
+
+        Invoke-Command -ScriptBlock {
+            SQLPackage /Action:DeployReport `
+            /SourceFile:$DACPAC_Path `
+            /TargetServerName:$TargetServer `
+            /TargetDatabaseName:$DatabaseName `
+            /OverwriteFiles:True `
+            /OutputPath:$DeployReportPath `
+            /Properties:DropObjectsNotInSource=True `
+            /Quiet:True
+        } -ErrorAction Stop
+
+        [xml]$x = gc -Path $DeployReportPath;
+        $x.DeploymentReport.Operations.Operation |
+        % -Begin {$a=@();} -process {$name = $_.name; $_.Item | %  {$r = New-Object PSObject -Property @{Operation=$name; Value = $_.Value; Type = $_.Type} ; $a += $r;} }  -End {$a}
+    }
+    catch {
+        $Result = 1
+        Write-Error ("Failed to generate deployment report: " + $_)
+        Exit 1
+    }
+}
+
+if ($Result -eq 0) {
+    try {
+        # Generate deployment script
+
+        Invoke-Command -ScriptBlock {
+            SQLPackage /Action:Script `
+            /SourceFile:$DACPAC_Path `
+            /TargetServerName:$TargetServer `
+            /TargetDatabaseName:$DatabaseName `
+            /OverwriteFiles:True `
+            /OutputPath:$DeployScriptPath `
+            /Properties:DropObjectsNotInSource=True `
+            /Quiet:True
+        } -ErrorAction Stop
+    }
+    catch {
+        $Result = 1
+        Write-Error ("Failed to generate deployment script: " + $_)
+        Exit 1
+    }
+}
+
+if ($Result -eq 0) {
+    try {
+        # Publish the DACPAC
+
+        Invoke-Command -ScriptBlock {
+            SQLPackage /Action:Publish `
+            /SourceFile:$DACPAC_Path `
+            /TargetServerName:$TargetServer `
+            /TargetDatabaseName:$DatabaseName `
+            /OverwriteFiles:True `
+            /DiagnosticsFile:DeployDiagnosticsPath `
+            /Properties:DropObjectsNotInSource=True `
+            /Quiet:True
+        } -ErrorAction Stop
+    }
+    catch {
+        $Result = 1
+        Write-Error ("Failed to publish DACPAC: " + $_)
+        Exit 1
+    }
+}
