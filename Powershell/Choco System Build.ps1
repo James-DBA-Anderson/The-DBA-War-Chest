ab737832eeaeb304019ae6817f37acbb0abada03
James Anderson <James-DBA-Anderson@users.noreply.github.com>
2018-02-28 13:07:36 +0000

    Reformat Choco System

--- Powershell/Choco System Build.ps1
+++ Powershell/Choco System Build.ps1
@@ -1,53 +1,70 @@
-﻿#Install Chocolatey
+#Install Chocolatey
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco feature enable -n allowGlobalConfirmation
 
-choco install googlechrome 
+# Basics
+
+#choco install googlechrome 
+choco install rdcman
+choco install cmder
+choco install 7zip.install
+choco install notepadplusplus
+choco install fileshredder
+
+#choco install avastfreeantivirus 
+#choco install avginternetsecurity
+
+
+# Chat
 #choco install skype
+#choco install slack
+#choco install microsoft-teams
+
+
+# File Sync
+choco install dropbox
+choco install onedrive
+
+
+# Development
+
 choco install git
 choco install poshgit 
-choco install dropbox
+choco install visualstudio2017community
+choco install sql-server-management-studio
+#choco install powerbi 
+
 choco install visualstudiocode
 choco install vscode-powershell
 choco install pester
-choco install rdcman
-choco install github
 #choco install docker
 #choco install NugetPackageExplorer
 #choco install vcredist2017
+#choco install vim
+
+#choco install agentransack
+#choco install ag # The Silver Searcher
+
+#choco install python
+#choco install python2
+#choco install pip
+#choco install pycharm-community
+#choco install anaconda3
+#choco install minikube 
 
 #choco install jdk9
 #choco install maven
 
-choco install zoomit
-choco install nginx 
-choco install vim
-choco install cmder
-choco install onedrive
-choco install slack
-choco install python
-#choco install python2
-choco install pip
-choco install pycharm-community
-choco install agentransack
-choco install 7zip.install
-choco install notepadplusplus 
-choco install audacity
-choco install steam
+
+# Presenting
+
+#choco install zoomit
+#choco install nginx 
+
+
+# Distractions
+
+#choco install audacity
+#choco install steam
 choco install spotify 
-
-choco install microsoft-teams
-
-choco install ag # The Silver Searcher
-choco install fileshredder
-
-#choco install avastfreeantivirus 
-choco install avginternetsecurity 
-
-#choco install anaconda3
-#choco install minikube 
-
-choco install visualstudio2017community
-choco install powerbi
-choco install sql-server-management-studio 
